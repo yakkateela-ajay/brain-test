1dd75cbc5b8155e8522431eb67891057dea921f2
yakkateela ajay <shockajay19@gmail.com>
2020-04-29 11:08:26 +0530

    braintest-updated

--- src/service/PersonalityCalculator.java
+++ src/service/PersonalityCalculator.java
@@ -3,23 +3,25 @@ package service;
 
 public class PersonalityCalculator
 {
-	public String findYourBrainType(String input)
+	public String findYourBrainType(String options)
 	{
-		System.out.println(input);
-		int x = findAnswers(input);
-		String str ="";
-	      if(x>=20 && x <= 55)
-	    	  str = "leftbrained";
-	      if(x>=56 && x <=64)
-	    	  str = "No clear preference";
-	      if(x>=65 && x<=100)
-	    	  str = "rightbrained";
-	      System.out.println(str);
-		return str;
+		System.out.println(options);
+		int x = findAnswers(options);
+		
+	      if(x>=20 && x <= 55){
+	    	  return "leftbrained";
+	      }
+	      else if(x>=56 && x <=64){
+	    	  return "noclearpreference";
+	      }
+	      else{
+	    	  return "rightbrained";
+	      }
+	
 	}
-	public int findAnswers(String input)
+	public int findAnswers(String options)
 	{
-		 String[] values = input.split(",");
+		 String[] values = options.split(",");
 	     int size = values.length;
 	     System.out.println(size);
 	      int [] array = new int [size];
@@ -29,9 +31,10 @@ public class PersonalityCalculator
 	      }
 	      int a = (array[0]+array[1]+array[2]+array[4]+array[7]+array[9]+array[10]+array[11]+array[13]+array[17]+array[19]);
 	      System.out.println(a);
-	      int b = array[3]+array[5]+array[6]+array[8]+array[12]+array[14]+array[15]+array[16]+array[18];
+	      int b = (array[3]+array[5]+array[6]+array[8]+array[12]+array[14]+array[15]+array[16]+array[18]);
 	      System.out.println(b);
 	      int x = 66 - a + b;
+	      System.out.println(x);
 	      return x;
 	}
 }
